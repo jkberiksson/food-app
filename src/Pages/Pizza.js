17cff2d3366f381a32b39c30d065dd6121443879
Jakob Eriksson <jkberiksson@gmail.com>
2022-07-03 16:16:51 +0200

    changed title

--- src/Pages/Pizza.js
+++ src/Pages/Pizza.js
@@ -1,6 +1,6 @@
-import { motion } from 'framer-motion';
-import Item from '../Components/Item';
-import styled from 'styled-components';
+import { motion } from "framer-motion";
+import Item from "../Components/Item";
+import styled from "styled-components";
 
 const Pizza = ({ items }) => {
   return (
